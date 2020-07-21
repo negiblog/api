d381db657ebdb9acc101b8dcc70918ddc5667258
negi <technegi109@gmail.com>
2020-07-21 10:21:40 +0530

    Updates

---
+++ precache-manifest.aada94a184292ae44297f75be7843549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1285849c9a16062289963fc280327be",
+    "url": "/api/index.html"
+  },
+  {
+    "revision": "1930fcb969377dd979b6",
+    "url": "/api/static/css/main.86d27e16.chunk.css"
+  },
+  {
+    "revision": "3dc83e10aaa44265b6df",
+    "url": "/api/static/js/2.2b8fc732.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api/static/js/2.2b8fc732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1930fcb969377dd979b6",
+    "url": "/api/static/js/main.d97cc251.chunk.js"
+  },
+  {
+    "revision": "bac320fade3cdf92217e",
+    "url": "/api/static/js/runtime-main.473e2bb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/api/static/media/logo.5d5d9eef.svg"
+  }
+]);
